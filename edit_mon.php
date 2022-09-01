d6940e361f299361c81e3f00afa7c7b8488abbfd
Christian Rodriguez <christiantrodriguez3@gmail.com>
2022-08-31 21:53:56 -0400

    push for laptop

--- edit_mon.php
+++ edit_mon.php
@@ -7,7 +7,10 @@
             require("depend\config.php");
             
             $cur_mon_id = $_POST['Id'];
-            $sql = "UPDATE monsters SET mon_desc='A small creature that likes to bite.' WHERE mon_id=2";
+            $name = $_POST['name'];
+            $owner = "Christian";
+            $desc = $_POST['desc'];
+            $sql = "UPDATE monsters SET mon_name='$name', mon_desc='$desc' WHERE mon_id=2";
             
             var_dump($db->query($sql));
             
--- monster_page.php
+++ monster_page.php
@@ -5,6 +5,7 @@
     <body>
         <?php
             require("depend\config.php");
+            $cur_user = "Christian";
             $cur_disp_id = $_POST['disp_id'];
             $cur_mon_id = $_POST['Id'];
             $sql = "SELECT * FROM monsters WHERE mon_id=$cur_mon_id Order by mon_id ASC";
@@ -14,10 +15,12 @@
                 // output data of each row
                 while ($row = $result->fetch_assoc()) {
                     echo "<h1> #" . $cur_disp_id . " " . $row['mon_name'] . "</h3>";
-                    echo '<form action="edit_mon_name.php" method="post"> <input type="text" id="Id" name="Id" style="visibility:hidden; height: 0px;" value= ' . $row['mon_id'] . '> <input type="submit" value="Edit Name" name="submit"> </form>';
                     echo "<h4>" . $row['mon_owner'] . "</h6>";
                     echo "<p>" . $row['mon_desc'] . "</p>";
-                    echo '<form action="delete_monster.php" method="post"> <input type="text" id="Id" name="Id" style="visibility:hidden; height: 0px;" value= ' . $row['mon_id'] . '> <input type="submit" value="Delete" name="submit"> </form>';
+                    if($cur_user == $row['mon_owner']){
+                        echo '<form action="edit_mon.php" method="post"> <input type="text" id="Id" name="Id" style="visibility:hidden; height: 0px;" value= ' . $row['mon_id'] . '> <input type="submit" value="Edit" name="submit"> </form>';
+                        echo '<form action="delete_monster.php" method="post"> <input type="text" id="Id" name="Id" style="visibility:hidden; height: 0px;" value= ' . $row['mon_id'] . '> <input type="submit" value="Delete" name="submit"> </form>';
+                    }
                 }
             }
             
